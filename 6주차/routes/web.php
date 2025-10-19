ec29b376ffa7131b902bce678cd60db47aeca03f
Yena <inasis@live.co.kr>
2025-10-19 22:12:52 +0900

    routes/web.php로 이름 변경

--- 6주차/routes/web.php
+++ 6주차/routes/web.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 use Fondue\Controller\PostController;
 
 return function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', function() {
+        return ['title'=>"Hello world", 'content'=>'Lorem ipsum dolor sit amet'];
+    });
+
     $r->addRoute('POST', '/post', [PostController::class, 'create']);
     $r->addRoute('GET', '/post/{id:\d+}', [PostController::class, 'read']);
     $r->addRoute('PUT', '/post', [PostController::class, 'update']);
