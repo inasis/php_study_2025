746120dde5514674098a59653e5007f8d95de10f
Yena <inasis@live.co.kr>
2025-10-19 22:15:30 +0900

    routes/web.php로 이름 변경, 라우트 핸들러 기능 확장
    라우팅 테이블에서 익명 함수가 사용되면 오류가 생기는 부분을 수정했습니다.

--- 6주차/src/Infrastructure/Routing/Router.php
+++ 6주차/src/Infrastructure/Routing/Router.php
@@ -12,9 +12,9 @@ use Psr\Container\ContainerInterface;
 
 class Router
 {
-    private ContainerInterface $container;
+    private ?Dispatcher $dispatcher = null;
     
-    public function __construct(ContainerInterface $container)
+    public function __construct(private ContainerInterface $container)
     {
         $this->container = $container;
     }
@@ -25,7 +25,7 @@ class Router
      * 
      * @param string $httpMethod HTTP 요청 메서드
      * @param string $uri 요청 URI
-     * @return mixed 컨트롤러 액션의 반환 값 (응답 데이터)
+     * @return mixed 핸들러의 반환값
      * @throws NotFoundException 라우트 매칭 실패 시
      * @throws MethodNotAllowedException HTTP 메서드가 허용되지 않을 시
      * @throws InternalServerErrorException 컨트롤러/액션 오류 시
@@ -33,50 +33,109 @@ class Router
     public function dispatch(string $httpMethod, string $uri): mixed
     {
         $uri = $this->sanitizeUri($uri);
+        $routeInfo = $this->getDispatcher()->dispatch($httpMethod, $uri);
         
-        // 요청 메서드와 URI를 기반으로 라우트 정의에서 일치하는 규칙을 검색합니다.
-        $routeInfo = $this->createDispatcher()->dispatch($httpMethod, $uri);
-        
-        // Match 절을 사용하여 라우트 정보를 직접 처리하고 컨트롤러를 실행합니다.
         return match ($routeInfo[0]) {
             Dispatcher::NOT_FOUND => throw new NotFoundException(),
             Dispatcher::METHOD_NOT_ALLOWED => throw new MethodNotAllowedException(),
-            Dispatcher::FOUND => (function () use ($routeInfo) {
-                // $routeInfo[1] = [$controllerClass, $action], $routeInfo[2] = $vars
-                [$controllerClass, $action] = $routeInfo[1];
-                $vars = $routeInfo[2];
-
-                // DI 컨테이너를 사용하여 컨트롤러 인스턴스를 가져옵니다.
-                $controller = $this->container->get($controllerClass);
-
-                if (!method_exists($controller, $action)) {
-                    // 라우트 설정 오류이므로 500 에러를 던집니다.
-                    throw new InternalServerErrorException("Controller action '{$controllerClass}::{$action}' not found.");
-                }
-
-                // 컨트롤러 액션을 호출하고 URI 매개변수($vars)를 전달합니다.
-                return $controller->{$action}($vars);
-            })(),
+            Dispatcher::FOUND => $this->handleFoundRoute($routeInfo[1], $routeInfo[2]),
         };
     }
     
+    /**
+     * 매칭된 라우트의 핸들러를 실행합니다.
+     * 
+     * @param mixed $handler 컨트롤러 배열 또는 클로저
+     * @param array $vars 라우트 파라미터
+     * @return mixed 핸들러의 반환값
+     * @throws InternalServerErrorException 잘못된 핸들러 타입일 때
+     */
+    private function handleFoundRoute(mixed $handler, array $vars): mixed
+    {
+        if (is_array($handler)) {
+            return $this->executeControllerAction($handler, $vars);
+        }
+        
+        if (is_callable($handler)) {
+            return $this->executeCallable($handler, $vars);
+        }
+        
+        throw new InternalServerErrorException("Invalid route handler.");
+    }
+    
+    /**
+     * 컨트롤러 액션을 실행합니다.
+     * 
+     * @param array $handler [컨트롤러 클래스명, 액션 메서드명]
+     * @param array $vars 라우트 파라미터
+     * @return mixed 컨트롤러 액션의 반환값
+     * @throws InternalServerErrorException 컨트롤러나 액션이 존재하지 않을 때
+     */
+    private function executeControllerAction(array $handler, array $vars): mixed
+    {
+        [$controllerClass, $action] = $handler;
+        
+        $controller = $this->container->get($controllerClass);
+
+        if (!method_exists($controller, $action)) {
+            throw new InternalServerErrorException(
+                "Controller action '{$controllerClass}::{$action}' not found."
+            );
+        }
+
+        return $controller->{$action}($vars);
+    }
+    
+    /**
+     * 클로저 핸들러를 실행합니다.
+     * 
+     * @param callable $handler 클로저 또는 callable
+     * @param array $vars 라우트 파라미터
+     * @return mixed 핸들러의 반환값
+     */
+    private function executeCallable(callable $handler, array $vars): mixed
+    {
+        return $handler(...array_values($vars));
+    }
+    
+    /**
+     * FastRoute Dispatcher 인스턴스를 가져옵니다.
+     * 인스턴스가 없으면 생성합니다.
+     * 
+     * @return Dispatcher
+     */
+    private function getDispatcher(): Dispatcher
+    {
+        if ($this->dispatcher === null) {
+            $this->dispatcher = $this->createDispatcher();
+        }
+        
+        return $this->dispatcher;
+    }
+    
+    /**
+     * FastRoute Dispatcher를 생성하고 라우트를 등록합니다.
+     * 
+     * @return Dispatcher
+     */
     private function createDispatcher(): Dispatcher
     {
         return \FastRoute\simpleDispatcher(function(RouteCollector $r) {
-            $routes = require __DIR__ . '/../../../config/routes.php';
+            $routes = require __DIR__ . '/../../../routes/web.php';
             $routes($r);
         });
     }
     
-   /**
+    /**
      * URI에서 쿼리 스트링을 제거하고 URL 인코딩을 디코딩하여
      * 라우팅 시스템이 처리할 수 있는 깔끔한 경로 문자열로 정제합니다.
      * 
      * @param string $uri 원본 URI
-     * @return string 정제된 URI 경로
+     * @return string 정제된 URI
      */
     private function sanitizeUri(string $uri): string
     {
+        // 쿼리 스트링 제거
         if (false !== $pos = strpos($uri, '?')) {
             $uri = substr($uri, 0, $pos);
         }
