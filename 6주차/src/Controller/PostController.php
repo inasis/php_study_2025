1d5b9e571f511886f49b28619a279be737560ba1
Yena <inasis@live.co.kr>
2025-10-21 00:09:38 +0900

    Validation Exception 위치 조정, DTO 위치 조정

--- 6주차/src/Controller/PostController.php
+++ 6주차/src/Controller/PostController.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 namespace Fondue\Controller;
 
 use Fondue\Service\PostService;
-use Fondue\DTO\PostCreateDTO;
-use Fondue\DTO\PostReadDTO;
-use Fondue\DTO\PostUpdateDTO;
-use Fondue\DTO\PostDeleteDTO;
+use Fondue\DTO\Post\PostCreateDTO;
+use Fondue\DTO\Post\PostReadDTO;
+use Fondue\DTO\Post\PostUpdateDTO;
+use Fondue\DTO\Post\PostDeleteDTO;
 use Fondue\DTO\Validation\Validator;
 
 class PostController
@@ -22,13 +22,13 @@ class PostController
      * 
      * @param array $data 요청 본문 데이터 (title, content)
      * @return array 생성된 게시글 데이터
-     * @throws \Fondue\Exception\Http\ValidationException 유효성 검사 실패 시
+     * @throws \Fondue\Exception\Validation\ValidationException 유효성 검사 실패 시
      */
     public function create(array $data = []): array
     {
         $dto = new PostCreateDTO(
             $data['title'] ?? '', 
-            $data['content'] ?? ''
+            $data['content'] ?? '',
         );
         $this->validator->validate($dto);
         return $this->postService->createPost($dto)->toArray();
@@ -39,12 +39,14 @@ class PostController
      * 
      * @param array $params 쿼리 또는 URL 파라미터 ({'id': int})
      * @return array 조회된 게시글 데이터
-     * @throws \Fondue\Exception\Http\ValidationException 유효성 검사 실패 시
+     * @throws \Fondue\Exception\Validation\ValidationException 유효성 검사 실패 시
      * @throws \Fondue\Exception\Http\NotFoundException 게시글을 찾을 수 없을 때
      */
     public function read(array $params = []): array
     {
-        $dto = new PostReadDTO((int)($params['id'] ?? 0));
+        $dto = new PostReadDTO(
+            (int)($params['id'] ?? 0),
+        );
         $this->validator->validate($dto);
         return $this->postService->readPost($dto)->toArray();
     }
@@ -54,7 +56,7 @@ class PostController
      * 
      * @param array $data 요청 본문 데이터 (id, title, content)
      * @return array 업데이트된 게시글 데이터
-     * @throws \Fondue\Exception\Http\ValidationException 유효성 검사 실패 시
+     * @throws \Fondue\Exception\Validation\ValidationException 유효성 검사 실패 시
      * @throws \Fondue\Exception\Http\NotFoundException 게시글을 찾을 수 없을 때
      */
     public function update(array $data = []): array
@@ -62,7 +64,7 @@ class PostController
         $dto = new PostUpdateDTO(
             (int)($data['id'] ?? 0),
             $data['title'] ?? null,
-            $data['content'] ?? null
+            $data['content'] ?? null,
         );
         $this->validator->validate($dto);
         return $this->postService->updatePost($dto)->toArray();
@@ -72,13 +74,15 @@ class PostController
      * 특정 ID의 게시글을 삭제합니다.
      * 
      * @param array $data 요청 본문 데이터 (id)
-     * @return bool 성공 여부 (HTTP 204 No Content를 위해 호출자에게 boolean 반환)
-     * @throws \Fondue\Exception\Http\ValidationException 유효성 검사 실패 시
+     * @return bool 성공 여부
+     * @throws \Fondue\Exception\Validation\ValidationException 유효성 검사 실패 시
      * @throws \Fondue\Exception\Http\NotFoundException 게시글을 찾을 수 없을 때
      */
     public function delete(array $data = []): bool
     {
-        $dto = new PostDeleteDTO((int)($data['id'] ?? 0));
+        $dto = new PostDeleteDTO(
+            (int)($data['id'] ?? 0),
+        );
         $this->validator->validate($dto);
         $this->postService->deletePost($dto);
         
---
+++ 6주차/src/DTO/Post/PostUpdateDTO.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Fondue\DTO\Post;
+
+use Fondue\DTO\Validation\Attribute\NotNull;
+use Fondue\DTO\Validation\Attribute\MinLength;
+
+readonly class PostUpdateDTO
+{
+    public function __construct(
+        #[NotNull]
+        public int $id,
+        #[MinLength(5)]
+        public ?string $title,
+        public ?string $content
+    ) {}
+
+    /**
+     * DTO의 public 속성을 포함하는 배열을 반환합니다.
+     * 
+     * * @return array
+     */
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+}
