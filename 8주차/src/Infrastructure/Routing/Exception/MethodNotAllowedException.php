e7d0decb43d115583d07b3b1fd4dd4116ad40dd1
inasis <inasis@live.co.kr>
2025-11-19 17:03:19 +0900

    Infrastructure Routing Exception 분리

---
+++ 8주차/src/Infrastructure/Routing/Exception/MethodNotAllowedException.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Hazelnut\Infrastructure\Routing\Exception;
+
+use Exception;
+
+class MethodNotAllowedException extends Exception
+{
+    public function __construct(
+        string $message = "Method Not Allowed",
+        int $code = 1000,
+        ?\Throwable $previous = null,
+        protected array $context = []
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->context = $context;
+    }
+
+    /**
+     * 컨텍스트 정보를 반환합니다
+     */
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    /**
+     * 컨텍스트 정보를 추가합니다
+     */
+    public function setContext(array $context): self
+    {
+        $this->context = array_merge($this->context, $context);
+        return $this;
+    }
+}
